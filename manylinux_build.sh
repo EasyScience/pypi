091a65ad17f98bf3747a2d18920b02a0044bdcb9
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2021-07-27 10:17:57 +0200

    Update manylinux_build.sh

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -14,28 +14,27 @@ function repair_wheel {
 # Install a system package required by our library
 #yum install -y atlas-devel
 # yum install -y libaec-dev libz-dev libsz2
-export PYBIN=/opt/python/cp38-cp38/bin
 
-
-"${PYBIN}/pip" install numpy scons
 mv /io/__init__.py /io/GSASII/
-cd /io/GSASII/fsource
-/opt/python/cp38-cp38/bin/scons
-cd  /io/GSASII
-rm -rf bindist
-mv ./bin ./bindist
-for n1 in bindist/*.so ; do
-   export n2="$( cut -d '.' -f 1 <<< "$n1" ).so"
-  mv ${n1} ${n2}
-done
-rm fsource/*.so
-rm fsource/*.a
 
 # Compile wheels
-#for PYBIN in /opt/python/*/bin; do
-"${PYBIN}/pip" wheel /io/ --no-deps --use-feature=in-tree-build -w /io/dist/
-rm -rf /io/build/
-#done
+for PYBIN in /opt/python/*/bin; do
+  "${PYBIN}/pip" install numpy scons
+  cd /io/GSASII/fsource
+  "${PYBIN}/scons"
+  cd  /io/GSASII
+  rm -rf bindist
+  mv ./bin ./bindist
+  for n1 in bindist/*.so ; do
+    export n2="$( cut -d '.' -f 1 <<< "$n1" ).so"
+    mv ${n1} ${n2}
+  done
+  rm fsource/*.so
+  rm fsource/*.a
+  "${PYBIN}/pip" wheel /io/ --no-deps --use-feature=in-tree-build -w /io/dist/
+  ls /io/dist/
+  rm -rf /io/build/
+done
 
 # Bundle external shared libraries into the wheels
 for whl in /io/dist/*.whl; do
@@ -43,6 +42,6 @@ for whl in /io/dist/*.whl; do
 done
 
 # Install packages and test
-#for PYBIN in /opt/python/*/bin/; do
-"${PYBIN}/pip" install GSASII --no-index -f /io/dist
-#done
+for PYBIN in /opt/python/*/bin/; do
+  "${PYBIN}/pip" install GSASII --no-index -f /io/dist
+done
