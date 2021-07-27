68db6fe5d4474cf1cb08efa657cb22f39a729b52
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2021-07-27 13:49:04 +0200

    Update manylinux_build.sh

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -20,19 +20,9 @@ mv /io/__init__.py /io/GSASII/
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" install numpy scons
-  cd /io/GSASII/fsource
-  "${PYBIN}/scons"
-  cd  /io/GSASII
-  rm -rf bindist
-  mv ./bin ./bindist
-  for n1 in bindist/*.so ; do
-    export n2="$( cut -d '.' -f 1 <<< "$n1" ).so"
-    mv ${n1} ${n2}
-  done
+  "${PYBIN}/pip" wheel /io/ --no-deps --use-feature=in-tree-build -w /io/dist/
   rm fsource/*.so
   rm fsource/*.a
-  "${PYBIN}/pip" wheel /io/ --no-deps --use-feature=in-tree-build -w /io/dist/
-  ls /io/dist/
   rm -rf /io/build/
 done
 
