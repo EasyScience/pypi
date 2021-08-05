33d4a443c2eddb1b213110969cbea1b280429303
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2021-08-05 09:47:39 +0200

    Try to fix the branch

---
+++ manylinux_build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e -u -x
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/dist/
+    fi
+}
+
+
+# Install a system package required by our library
+#yum install -y atlas-devel
+# yum install -y libaec-dev libz-dev libsz2
+
+mv /io/__init__.py /io/GSASII/
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+  "${PYBIN}/pip" install numpy scons
+  export SCONS_PATH=${PYBIN}/scons
+  cd /io
+  ${PYBIN}/python /io/setup.py bdist_wheel
+  rm -rf /io/build/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in /io/dist/*.whl; do
+    repair_wheel "$whl"
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+  "${PYBIN}/pip" install GSASII --no-index -f /io/dist
+  # "${PYBIN}/python" -c 'from GSASII import GSASIIscriptable as GS2; exit()'
+done
---
+++ setup.py
@@ -0,0 +1,128 @@
+__author__ = 'github.com/wardsimon'
+__version__ = '0.0.1'
+
+import os
+import shutil
+import subprocess
+import sys
+from distutils.command.install_data import install_data
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from setuptools.command.install_lib import install_lib
+
+
+class SconsExtension(Extension):
+    """
+    An extension to run the scons build
+    This simply overrides the base extension class so that setuptools
+    doesn't try to build your sources for you
+    """
+
+    def __init__(self, name, sources=[]):
+        super().__init__(name=name, sources=sources)
+
+
+class BuildSconsLibs(build_ext):
+
+    def run(self):
+        for extension in self.extensions:
+            print(" >>> BUILDING EXTENSION - {}".format(str(extension)))
+            self.build_scons(extension)
+
+    def build_scons(self, extension: Extension):
+        print("Calling SCons to build the module")
+        src_dir = os.path.join(self.build_lib, 'GSASII', 'fsource')
+        print("SRC DIR: {}".format(src_dir))
+        scons_path = [os.environ.get('SCONS_PATH', 'scons')]
+        scons_path = scons_path + scons_vars
+        print("SCONS_PATH = {}".format(scons_path))
+        subprocess.check_call(scons_path, cwd=src_dir)
+        bin_dir = os.path.join(self.build_lib, 'GSASII', 'bindist')
+        os.makedirs(bin_dir, exist_ok=True)
+        print("made bin_dir = {}".format(bin_dir))
+        self.distribution.bin_dir = bin_dir
+
+
+class InstallSconsLibsData(install_data):
+    """
+    Just a wrapper to get the install data into the egg-info
+    Listing the installed files in the egg-info guarantees that
+    all of the package files will be uninstalled when the user
+    uninstalls your package through pip
+    """
+
+    def run(self):
+        """
+        Out files are the libraries that were built using cmake
+        """
+        self.outfiles = self.distribution.data_files
+
+
+class InstallSconsLibs(install_lib):
+    """
+    Get the libraries from the parent distribution, use those as the outfiles
+    Skip building anything; everything is already built, forward libraries to
+    the installation step
+    """
+
+    def run(self):
+        """
+        Copy libraries from the bin directory and place them as appropriate
+        """
+
+        self.announce("Moving library files", level=3)
+        # We have already built the libraries in the previous build_ext step
+        self.skip_build = True
+        # Move the lib to the correct location.
+        bin_dir = os.path.join(self.build_dir, 'GSASII', 'bin')
+        install_dir = os.path.join(self.build_dir, 'GSASII', 'bindist')
+        for i in os.listdir(bin_dir):
+            shutil.move(os.path.join(bin_dir, i), install_dir)
+        src_dir = os.path.join(self.build_dir, 'GSASII', 'fsource')
+        print(">>> src_dir in run  = {}".format(src_dir))
+        tmp_files = [os.path.join(src_dir, _pyd) for _pyd in
+                    os.listdir(src_dir) if
+                    os.path.isfile(os.path.join(src_dir, _pyd)) and
+                    os.path.splitext(_pyd)[1] in [".pyd", ".so", '.o', '.a']]
+        for file in tmp_files:
+            os.remove(file)
+
+        # Must be forced to run after adding the libs to data_files
+        self.distribution.run_command("install_data")
+        super().run()
+
+
+scons_vars = []
+if "--scons_args" in sys.argv:
+    idx = sys.argv.index("--scons_args")
+    sys.argv.remove("--scons_args")
+    for opt_idx in range(idx, len(sys.argv)):
+        arg = sys.argv[opt_idx]
+        scons_vars.append(arg)
+    sys.argv = sys.argv[0:idx]
+
+setup(
+    name='GSASII',
+    version='0.0.1',
+    packages=['GSASII', 'GSASII.CifFile', 'GSASII.fsource', 'GSASII.exports', 'GSASII.NIST_profile'],
+    url='https://subversion.xray.aps.anl.gov/pyGSAS/',
+    ext_modules=[SconsExtension(name='GSASII')],
+    license='BSD3',
+    author='simonward',
+    author_email='',
+    description='',
+    include_package_data=True,
+    package_data={
+        'GSASII': ['*'],
+    },
+    cmdclass={
+        'build_ext':    BuildSconsLibs,
+        'install_data': InstallSconsLibsData,
+        'install_lib':  InstallSconsLibs,
+
+    },
+    setup_requires=['setuptools', 'wheel', 'scons', 'numpy'],
+    install_requires=['numpy'],
+
+)
